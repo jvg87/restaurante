272ba835e11bd1a41ab2a645a5dd0f24201f38db
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-17 16:04:20 -0300

    criando e migrando os models

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ nome: 'Carlinhos' })
+  return res.json({ nome: 'Juquinha' })
 })
 
 export { router };
