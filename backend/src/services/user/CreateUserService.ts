a439f54f516dc505684933266c1c5a30694c38ef
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-18 17:50:30 -0300

    cadastrando usuario no BD

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from "../../prisma";
 
 interface UserRequest{
   name: string;
@@ -7,8 +8,35 @@ interface UserRequest{
 
 class CreateUserService{
   async execute({ name, email, password }: UserRequest){
-    console.log(name);
-    return { name: name }
+    
+    if (!email){
+      throw new Error('Email incorrect')
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
+        email: email
+      }
+    })
+
+    if (userAlreadyExists){
+      throw new Error('User already exists')
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      }
+    })
+
+    return user;
   }
 }
 
