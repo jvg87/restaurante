081d1a5d62980e499e311107a4bbafcdfa1a4ffc
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-19 00:32:54 -0300

    gerando token JWT

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { CreateUserController } from './controllers/user/CreateUserController';
-import { AuthUserController } from './controllers/user/AuthUserController';
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
 // -- Rotas User --
-router.post('/users', new CreateUserController().handle)
-router.post('/session', new AuthUserController().handle)
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,36 +1,52 @@
 import prismaClient from "../../prisma";
-import { compare } from 'bcryptjs';
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
-interface AuthRequest{
+interface AuthRequest {
   email: string;
   password: string;
 }
 
-class AuthUserService{
-  async execute({ email, password }: AuthRequest){
-
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
     // Verificar se email existe
     const user = await prismaClient.user.findFirst({
       where: {
-        email: email
-      }
-    })
-    
-    if (!user){
-      throw new Error('User/Password incorrect')
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User/Password incorrect");
     }
 
     // Verificar se a senha está correta
-    const passwordMatch = await compare(password, user.password)
+    const passwordMatch = await compare(password, user.password);
 
-    if (!passwordMatch){
-      throw new Error('User/Password incorrect')
+    if (!passwordMatch) {
+      throw new Error("User/Password incorrect");
     }
 
+    // Gerar token JWT
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
 
-    return { ok: true }
-
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
 
-export {AuthUserService};
+export { AuthUserService };
