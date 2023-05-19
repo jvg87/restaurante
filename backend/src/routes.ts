57cd7ebe08376a3aec3be280c8c96504e5512ae1
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-19 14:42:32 -0300

    cadastrando categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -12,5 +14,8 @@ const router = Router();
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// -- Rotas Category --
+router.post("/category", isAuthenticated, new CreateCategoryController().handle)
 
 export { router };
