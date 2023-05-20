3bf3248a6cbb4f476854c3d61b40ab981eaf825a
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-19 23:46:36 -0300

    add items numa order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from "./controllers/order/CreaterOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -38,6 +39,7 @@ router.get("/category/product", isAuthenticated, new ListByCategoryController().
 // -- Rotas Order --
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
 
 
 export { router };
