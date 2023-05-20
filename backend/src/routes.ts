2e2aa48f17ff0fb99db396895cdca22394b07ba1
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-19 22:38:10 -0300

    filtrando produtos por categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 import { CreateProductController } from "./controllers/product/CreateProductController";
 
@@ -29,6 +30,7 @@ router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 // -- Rotas Product --
 router.post("/product", isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.get("/category/product", isAuthenticated, new ListByCategoryController().handle);
 
 
 export { router };
