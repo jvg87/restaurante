2563f97825c5f6eeba31f18b107b72ce414b4cd9
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-22 14:17:11 -0300

    atualizando status do draft

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -15,6 +15,7 @@ import { CreateOrderController } from "./controllers/order/CreaterOrderControlle
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -42,6 +43,7 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
 router.post('/order/add', isAuthenticated, new AddItemController().handle);
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
+router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 
 
 export { router };
