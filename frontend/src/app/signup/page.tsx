9ee9892733985c9616247837009b47bda8381213
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-26 15:49:24 -0300

    feat: created signup page

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,18 +1,30 @@
-import styles from '../styles/page.module.scss';
-import { laila } from './fonts';
+import Link from 'next/link';
+import styles from '../../styles/page.module.scss';
+import { laila } from '../fonts';
+
 import { Input } from '@/components/ui/Input';
 import { Button } from '@/components/ui/Butotn';
-import Link from 'next/link';
 
-export default function Home() {
-  return (
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Faça seu cadastro agora!'
+}
+
+function SignUp() {
+  return ( 
     <main className={styles.container_center}>
       <h1 className={laila.className}>
         <span>Food</span>
         House
       </h1>
       <div className={styles.login}>
+        <h1>Criando sua conta</h1>
         <form>
+          <Input 
+            placeholder='Digite seu nome'
+            type='text'  
+          />
           <Input 
             placeholder='Digite seu email'
             type='text'  
@@ -25,14 +37,16 @@ export default function Home() {
             type='submit'
             loading={false}
           >
-            Acessar
+            Cadastrar
           </Button>
         </form>
-        <Link href='/signup' className={styles.text}>
-          Não possui uma conta? Cadastre-se
+        <Link href='/' className={styles.text}>
+          Já possui uma conta? Faça login!
         </Link>
 
       </div>
     </main>
-  )
+  );
 }
+
+export default SignUp;
