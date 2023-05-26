55566621e7eae9184c73f6435c7367b9a2e2d687
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-26 14:15:08 -0300

    feat: created component input

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,25 @@
-
+import styles from '../styles/page.module.scss';
+import { laila } from './fonts';
+import { Input } from '@/components/ui/Input';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Teste inicial :)</h1>
+    <main className={styles.container_center}>
+      <h1 className={laila.className}>
+        <span>Food</span>
+        House
+      </h1>
+      <div className={styles.login}>
+        <form>
+          <Input 
+            placeholder='Digite seu email'
+            type='text'  
+          />
+          <Input
+            type='password'
+          />
+        </form>
+      </div>
     </main>
   )
 }
