881af7e3351b3403a0360f34214f318c094d48c9
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-27 09:39:57 -0300

    fix: restarting the project

--- frontend/src/app/fonts.tsx
+++
@@ -1,5 +0,0 @@
-import { Lato, Laila } from 'next/font/google';
-
-export const lato = Lato({ weight:["100", "300", '400','700', '900'], subsets: ['latin'] });
-
-export const laila = Laila({weight: ['300', '400', '500', '600', '700'], subsets:['latin']});
