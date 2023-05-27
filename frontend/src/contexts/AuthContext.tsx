2059c0382db3486584d94ab8272c0f442c6829e2
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-26 22:09:36 -0300

    feat: config API

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,10 +1,14 @@
 "use client"
+
 import { createContext, ReactNode, useState } from 'react'; 
+import { destroyCookie } from 'nookies';
+import Router from 'next/router';
 
 type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
+  signOut: () => void;
 }
 
 type UserProps = {
@@ -22,7 +26,16 @@ type AuthProviderProps = {
   children: ReactNode;
 }
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
+
+export function signOut(){
+  try{
+    destroyCookie(undefined, '@nextauth.token')
+    Router.push('/')
+  } catch {
+    console.log('erro ao deslogar');
+  }
+}
 
 export function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<UserProps>();
@@ -33,7 +46,7 @@ export function AuthProvider({ children }: AuthProviderProps){
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   )
