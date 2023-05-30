dd7baf1b88aa4cd9d94e52871804df8d515aa3e2
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-29 23:57:14 -0300

    feat: creating modalOrder

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Header } from "@/components/Header";
 import { canSSRAuth } from "@/utils/canSSRAuth";
 import Head from "next/head";
@@ -5,11 +6,74 @@ import Head from "next/head";
 import styles from './styles.module.scss';
 import { FiRefreshCcw } from 'react-icons/fi';
 
+import { setupAPIClient } from "@/services/api";
+
+import Modal from 'react-modal';
+
 import { Laila } from 'next/font/google';
+import { ModalOrder } from '@/components/ModalOrder';
 
 const laila = Laila({ weight:['300', '400', '500', '600', '700'], subsets: ['latin'] });
 
-function dashboard() {
+type OrderProps = {
+  id: string;
+  table: number | string;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+}
+
+interface HomeProps{
+  orders: OrderProps[];
+}
+
+export type OrderItemProps = {
+  id: string;
+  amount: number;
+  order_id: string;
+  product_id: string;
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    banner: string;
+  }
+  order:{
+    id: string;
+    table:string | number;
+    status: boolean;
+    name: string | null;
+  }
+}
+
+function dashboard({ orders }: HomeProps) {
+  const [orderList, setOrderList] = useState(orders || []);
+
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function handleCloseModal(){
+    setModalVisible(false);
+  }
+
+  async function handleOpenModalView(id: string){
+
+    const apiClient = setupAPIClient();
+
+    const response = await apiClient.get('/order/detail', {
+      params: {
+        order_id: id
+      }
+    })
+
+    setModalItem(response.data);
+    setModalVisible(true);
+
+  }
+
+  Modal.setAppElement('#__next');
+  
   return (  
     <>
       <Head>
@@ -27,25 +91,23 @@ function dashboard() {
           </div>
 
           <article className={styles.list_orders}>
-            <section className={styles.order_item}>
-              <button>
-                <div className={styles.tag}>
-                </div>
-                <span>Mesa 30</span>
-              </button>
-            </section>
-          </article>
-          <article className={styles.list_orders}>
-            <section className={styles.order_item}>
-              <button>
-                <div className={styles.tag}>
-                </div>
-                <span>Mesa 30</span>
-              </button>
-            </section>
-          </article>
 
+            {orderList.map((item)=> (
+              <section key={item.id} className={styles.order_item}>
+                <button onClick={ () => handleOpenModalView(item.id)}>
+                  <div className={styles.tag}>
+                  </div>
+                  <span>Mesa {item.table}</span>
+                </button>
+              </section>
+            ))}
+            
+          </article>
         </main>
+
+        { modalVisible && (
+          <ModalOrder/>
+        )}
 
       </div>
     </>
@@ -56,7 +118,13 @@ export default dashboard;
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
 
+  const apiClient = setupAPIClient(ctx);
+
+  const response = await apiClient.get('/orders');
+
   return {
-    props: {}
+    props: {
+      orders: response.data
+    }
   }
 })
